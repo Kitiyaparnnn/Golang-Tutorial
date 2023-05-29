f2eb11ed33e97bb45bad0a93de0a4d316dec6c3e
Kitiyaparnnn <parn.takhum@gmail.com>
2023-05-29 22:39:28 +0700

    route with param - url path

--- main.go
+++ main.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type Course struct {
-	Id 		int `json: "id"`
-	Name 	string `json: "name"`
-	Price 	float64 `json: "price"`
-	Instructor string `json: "instructor"`
+	Id         int     `json: "id"`
+	Name       string  `json: "name"`
+	Price      float64 `json: "price"`
+	Instructor string  `json: "instructor"`
 }
 
 var CourseList []Course
 
-func init()  {
+func init() {
 	CourseJSON := `[
 		{
 			"id": 1,
@@ -32,8 +35,8 @@ func init()  {
 		}
 	]`
 
-	err := json.Unmarshal([]byte(CourseJSON), &CourseList)
-	if err != nil{
+	err := json.Unmarshal([]byte(CourseJSON), &CourseList) //json->object
+	if err != nil {
 		log.Fatal(err)
 	}
 }
@@ -41,14 +44,74 @@ func init()  {
 func getNextId() int {
 	highestId := -1
 	for _, course := range CourseList {
-		if highestId < course.Id{
+		if highestId < course.Id {
 			highestId = course.Id
 		}
 	}
 	return highestId + 1
 }
 
+func findId(id int) (*Course, int) {
+	for i, course := range CourseList {
+		if course.Id == id {
+			return &course, i
+		}
+	}
+	return nil, 0
+}
+
 func courseHandler(w http.ResponseWriter, r *http.Request) {
+	urlPathSegments := strings.Split(r.URL.Path, "course/")
+	id, err := strconv.Atoi(urlPathSegments[len(urlPathSegments)-1])
+	if err != nil {
+		log.Print(err)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	course, index := findId(id)
+	if course == nil {
+		http.Error(w, fmt.Sprintf("no course with id %d", id), http.StatusNotFound)
+		return
+	}
+	switch r.Method {
+	case http.MethodGet:
+		courseJSON, err := json.Marshal(course) //object->json
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-type", "application/json")
+		w.Write(courseJSON)
+	case http.MethodPut:
+		var updateCourse Course
+		byteBody, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = json.Unmarshal(byteBody, &updateCourse)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if updateCourse.Id != id {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		course = &updateCourse
+		CourseList[index] = *course
+		updated, _ := json.Marshal(course)
+		w.Header().Set("Content-type", "application/json")
+		w.Write(updated)
+		w.WriteHeader(http.StatusOK)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+
+}
+
+func coursesHandler(w http.ResponseWriter, r *http.Request) {
 	courseJSON, err := json.Marshal(CourseList)
 	switch r.Method {
 	case http.MethodGet:
@@ -79,6 +142,7 @@ func courseHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// fmt.Println("Hello go")
 
-	http.HandleFunc("/course", courseHandler)
+	http.HandleFunc("/course/", courseHandler)
+	http.HandleFunc("/course", coursesHandler)
 	http.ListenAndServe(":8000", nil)
 }
